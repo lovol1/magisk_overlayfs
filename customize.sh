4bb78feeebe1f3c58848e776dc03a4127824deae
lovol1 <114041917+lovol1@users.noreply.github.com>
2024-03-06 21:29:45 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -131,3 +131,15 @@ if [ "$BOOTMODE" ] && [ -z "$KSU" ]; then
 fi
 
 ui_print
+
+OVERLAY_IMAGE_EXTRA=0     # number of kb need to be added to overlay.img
+OVERLAY_IMAGE_SHRINK=true # shrink overlay.img or not?
+INCLUDE_MAGIC_MOUNT=false # enable legacy Magisk mount
+
+# Only use OverlayFS if Magisk_OverlayFS is installed
+if [ -f "/data/adb/modules/magisk_overlayfs/util_functions.sh" ] && \
+    /data/adb/modules/magisk_overlayfs/overlayfs_system --test; then
+  ui_print "- Add support for overlayfs"
+  . /data/adb/modules/magisk_overlayfs/util_functions.sh
+  support_overlayfs && rm -rf "$MODPATH"/system
+fi
